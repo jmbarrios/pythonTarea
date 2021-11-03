6abb4660f6a4992281076e52cefd1d0222477cb6
Juan Martín Barrios <j.m.barrios@gmail.com>
2021-11-02 22:54:43 -0600

    Config files

---
+++ homework_test.py
@@ -0,0 +1,7 @@
+import homework
+
+def test_suma():
+    assert homework.suma(1, 3) == 4
+
+def test_saludo():
+    assert homework.saludo('Juan') == 'Hola Juan'
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip3 install pytest git+https://github.com/jmbarrios/hw-conversion.git
+
+# Convert homework to script
+hwconvert
